2176da3514b35c434d77ceeebb3e36dd88f505e9
GabsLoops <gabslopes34@gmail.com>
2020-07-27 23:05:55 -0300

    commit inicial

---
+++ src/components/Menu/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Logo from  '../../assets/images/logo.codeflix.png'
+import './Menu.css'
+import Button from '../Button'
+//import ButtonLink from '../ButtonLink'
+
+function Menu() {
+    return (
+        <nav className="Menu">
+            <a href="/">
+                <img className="Logo" src={Logo} alt="CodeFlix Logo"/>
+            </a>
+            <Button as="a" className="ButtonLink" href="/">
+                Novo vídeo
+            </Button>
+        </nav>  
+    )
+}
+
+export default Menu
