3b255eb1ae749ba52eed3738c3bad497337c847f
GabsLoops <gabslopes34@gmail.com>
2020-07-28 22:55:22 -0300

    Commit Aula 2

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -2,15 +2,16 @@ import React from 'react'
 import Logo from  '../../assets/images/logo.codeflix.png'
 import './Menu.css'
 import Button from '../Button'
+import { Link } from 'react-router-dom'
 //import ButtonLink from '../ButtonLink'
 
 function Menu() {
     return (
         <nav className="Menu">
-            <a href="/">
+            <Link to="/">
                 <img className="Logo" src={Logo} alt="CodeFlix Logo"/>
-            </a>
-            <Button as="a" className="ButtonLink" href="/">
+            </Link>
+            <Button as={Link} className="ButtonLink" to="/cadastro/video">
                 Novo vídeo
             </Button>
         </nav>  
